715cf046902aa72be14bcbdad714c5744eca88ed
felipeddsantos <82002471+felipeddsantos@users.noreply.github.com>
2021-06-13 23:49:08 -0300

    final

--- Ratis/src/main/java/MaquinaDeEstados.java
+++ Ratis/src/main/java/MaquinaDeEstados.java
@@ -4,18 +4,19 @@ import org.apache.ratis.statemachine.TransactionContext;
 import org.apache.ratis.statemachine.impl.BaseStateMachine;
 
 import java.nio.charset.Charset;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class MaquinaDeEstados extends BaseStateMachine
 {
-    private final Map<String, String> key2values = new ConcurrentHashMap<>();
+    private final Map<String, String> clients = new ConcurrentHashMap<>();
 
     @Override
     public CompletableFuture<Message> query(Message request){
         final String[] opKey = request.getContent().toString(Charset.defaultCharset()).split(":");
-        final String result = opKey[0]+ ":"+ key2values.get(opKey[1]);
+        final String result = opKey[0]+ ":"+ clients.get(opKey[1]);
 
         LOG.info("{}: {} = {}", opKey[0], opKey[1], result);
         return CompletableFuture.completedFuture(Message.valueOf(result));
@@ -26,31 +27,35 @@ public class MaquinaDeEstados extends BaseStateMachine
         
         final RaftProtos.LogEntryProto entry = trx.getLogEntry();
         final String[] opKeyValue = entry.getStateMachineLogEntry().getLogData().toString(Charset.defaultCharset()).split(":");
-        String result;
         final RaftProtos.RaftPeerRole role = trx.getServerRole();
+
+        String result;
         
-        if(opKeyValue[0].equals("add")){
+        if(opKeyValue[0].equals("add_client")){
             
-            result = "add:" + key2values.put(opKeyValue[1], opKeyValue[2]);
+            clients.put(opKeyValue[1], opKeyValue[2]);
+            result = "add_client," + opKeyValue[1] + "," + opKeyValue[2];
             LOG.info("{}:{} {} {}={}", role, getId(), opKeyValue[0], opKeyValue[1], opKeyValue[2]);
         }
         
-        else if(opKeyValue[0].equals("delete")){
+        else if(opKeyValue[0].equals("delete_client")){
             
-            result = "delete:" + key2values.remove(opKeyValue[1]);
-            LOG.info("{}:{} {} {}={}", role, getId(), opKeyValue[0], opKeyValue[1], result);
+            clients.remove(opKeyValue[1]);
+            result = "delete_client," + opKeyValue[1];
+            LOG.info("{}:{} {} {}", role, getId(), opKeyValue[0], opKeyValue[1]);
         }
             
         else{
             
-            result = "update:"+ key2values.replace(opKeyValue[1], opKeyValue[2]);
+            clients.replace(opKeyValue[1], opKeyValue[2]);
+            result = "update_client," + opKeyValue[1] + "," + opKeyValue[2];
             LOG.info("{}:{} {} {}={}", role, getId(), opKeyValue[0], opKeyValue[1], opKeyValue[2]);
         }
-        
+
         final CompletableFuture<Message> f = CompletableFuture.completedFuture(Message.valueOf(result));
 
         if (LOG.isTraceEnabled()) {
-            LOG.trace("{}: key/values={}", getId(), key2values);
+            LOG.trace("{}: key/values={}", getId(), clients);
         }
  
         return f;
