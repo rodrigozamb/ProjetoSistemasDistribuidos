3440fd7fc5a1b49bdc5ab6c63aa4ddec53df4cc0
felipeddsantos <82002471+felipeddsantos@users.noreply.github.com>
2021-06-13 23:47:01 -0300

    final

--- portalCliente.py
+++ portalCliente.py
@@ -1,66 +1,98 @@
-# server.py
-#!/usr/bin/python                           # This is server.py file
+#Removendo comunicação por mqttp
 
-import socket                               # Import socket module
+#import paho.mqtt.client as mqtt
+#def on_message(client, userdata, message):
+#   global users
+
+#   print("\n",str(message.payload.decode("utf-8")))
+#   msg = []
+#   msg = str(message.payload.decode("utf-8")).strip().split(",")
+
+#   if msg[0] == "I":
+#      if msg[0] not in users.keys():
+#         users[msg[1]] = msg[2]
+#   elif msg[0] == "U":
+#      users[msg[1]] = msg[2]
+#   elif msg[0] == "D":
+#      del users[msg[1]]
+      
+#   print(users)
+#   if message.retain == 1:
+#      print("This is a retained message")
+
+#broker = "localhost"
+#client = mqtt.Client("client")
+#print("connecting to broker")
+#client.connect(broker)
+
+#def subscribeToTopic():
+#   global client
+#   client.subscribe("/data", 0)
+#   client.on_message = on_message
+#   while True: 
+#      client.loop()
+
+import socket  
 import threading
+import struct
 from config import *
-####################################
-import paho.mqtt.client as mqtt
 import time
 from clienteMenuEnum import clienteMenuEnum 
 
 database = dict([])
 users = dict([])
+
 def connect_portal(host,port):
    print("attemp to connect Portal with port - ",port)
    try:
-      s.bind((host, port))                        # Bind to the port
+      s.bind((host, port))                      
    except:
       return connect_portal(host,port+1)
    return host,port
 
-def on_message(client, userdata, message):
+####################################
+
+s = socket.socket()
+portal_host, portal_port = connect_portal(host,port)
+
+#Atualização do banco de dados
+def updateData(data):
+   
    global users
 
-   print("\n",str(message.payload.decode("utf-8")))
    msg = []
-   msg = str(message.payload.decode("utf-8")).strip().split(",")
-
-   if msg[0] == "I":
-      if msg[0] not in users.keys():
+   msg = str(data).split(",")
+   
+   #Extraindo informações
+   msg[0] = msg[0].split("'")[1]
+   msg[1] = int(msg[1].split("'")[0])
+   msg[2] = msg[2].split("'")[0]
+   
+   if msg[0] == "add_client":
+      if msg[1] not in users.keys():
          users[msg[1]] = msg[2]
-   elif msg[0] == "U":
+   elif msg[0] == "update_client":
       users[msg[1]] = msg[2]
-   elif msg[0] == "D":
+   elif msg[0] == "delete_client":
       del users[msg[1]]
       
    print(users)
-   if message.retain == 1:
-      print("This is a retained message")
+   print(database)
+   
+#Recebendo atualização do banco do processo responsável pelo Ratis, requisitado pelos outros portais
+#Informações recebidas em (224.1.1.1, 5101)
+def ratisReceive():
+  
+  while True:
+    
+    r_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    r_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    r_socket.bind(("224.1.1.1", 5101))
 
-
-broker = "localhost"
-client = mqtt.Client("client")
-print("connecting to broker")
-client.connect(broker)
-####################################
-
-s = socket.socket()                         # Create a socket object
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Host e porta agora vem do arquivo de configuração "config.py" #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-# host = socket.gethostname()                 # Get local machine name
-# host = "localhost"                          # Get local machine name
-# port = 12345                                # Reserve a port for your service.
-portal_host, portal_port = connect_portal(host,port)
-
-
-def subscribeToTopic():
-   global client
-   client.subscribe("/data", 0)
-   client.on_message = on_message
-   while True: 
-      client.loop()
+    req = struct.pack("=4sl", socket.inet_aton("224.1.1.1"), socket.INADDR_ANY)
+    r_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, req)
+    data = r_socket.recvfrom(1024)
+    updateData(data)
 
 def handle_client(c, addr):
    global database
@@ -83,6 +115,7 @@ def handle_client(c, addr):
                idata = c.recv(packageSize)
                insertData = idata.decode()
                insertData = insertData.split(",")
+               insertData[0] = int(insertData[0])
 
                if len(insertData) == 3:
                   if insertData[0] not in users.keys():
@@ -92,8 +125,8 @@ def handle_client(c, addr):
                      if insertData[0] not in database.keys():
                         database[insertData[0]] = []
 
-                     database[insertData[0]].append(
-                        (insertData[1], insertData[2]))
+                     database[insertData[0]].append((insertData[1], insertData[2]))
+                     
                      print(database)
                      c.send("Operation Success".encode())
                else:
@@ -105,6 +138,7 @@ def handle_client(c, addr):
                idata = c.recv(packageSize)
                insertData = idata.decode()
                insertData = insertData.split(",")
+               insertData[0] = int(insertData[0])
 
                if len(insertData) == 3:
                   if insertData[0] not in users.keys():
@@ -136,23 +170,24 @@ def handle_client(c, addr):
                # listar tarefas
                idata = c.recv(packageSize)
                insertData = idata.decode()
+               insertData = insertData.split(",")
+               insertData[0] = int(insertData[0])
 
-               userid = insertData
                exists = False
 
-               if len(userid) == 1:
+               if len(insertData) == 1:
                   if insertData[0] not in users.keys():
                      c.send(
                      "Operation Fail - invalid CID".encode())
                   else:
-                     if userid not in users.keys():
+                     if insertData[0] not in users.keys():
                         c.send(
                         "Operation Fail - invalid CID".encode())
                      else:
-                        if userid not in database.keys():
+                        if insertData[0] not in database.keys():
                            c.send("User does not have tasks".encode())
                         else:
-                           resp = str(database[userid])
+                           resp = str(database[insertData[0]])
                            c.send(resp.encode())
 
                else:
@@ -163,18 +198,18 @@ def handle_client(c, addr):
                # apagar todas tarefas
                idata = c.recv(packageSize)
                insertData = idata.decode()
+               insertData = insertData.split(",")
+               insertData[0] = int(insertData[0])
 
-               userid = insertData
-
-               if len(userid) == 1:
+               if len(insertData) == 1:
                   if insertData[0] not in users.keys():
                      c.send(
                      "Operation Fail - invalid CID".encode())
                   else:
-                     if userid not in database.keys():
+                     if insertData[0] not in database.keys():
                         c.send("Operation Fail - User does not have tasks".encode())
                      else:
-                        database[userid] = []
+                        database[insertData[0]] = []
                         c.send("Operation Success".encode())
                else:
                   c.send(
@@ -185,6 +220,7 @@ def handle_client(c, addr):
                idata = c.recv(packageSize)
                insertData = idata.decode()
                insertData = insertData.split(",")
+               insertData[0] = int(insertData[0])
 
                if len(insertData) == 2:
                   if insertData[0] not in users.keys():
@@ -206,6 +242,7 @@ def handle_client(c, addr):
                            c.send("Operation Fail - Task not found".encode())
                      else:
                            del database[userid][index]
+                           
                            c.send("Operation Success".encode())
                else:
                   c.send(
@@ -216,15 +253,16 @@ def handle_client(c, addr):
    except:
       print("An internal error occoured and "+str(addr)+" got disconected")
 
-
 def start():
+   
+   #Thread para comunicação com ratis
+   threadsub = threading.Thread(target=ratisReceive, args=())
+   threadsub.start()
+    
    # Now wait for client connections.
    s.listen(5)
    global portal_port
    print("[LISTENING] Server is listening on "+str(portal_port))
-
-   threadsub = threading.Thread(target=subscribeToTopic, args=())
-   threadsub.start()
    
    while True:
       # Establish connection with client.
@@ -233,7 +271,6 @@ def start():
       thread.start()
       print("[ACTIVE CONNECTIONS] "+str((threading.activeCount()-1)))
    s.close()
-
 
 print("[STARTING] server is starting...")
 start()
