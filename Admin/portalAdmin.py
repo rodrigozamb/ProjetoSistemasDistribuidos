6fb0add563150ec6676f0395388c8d402e682ebb
felipeddsantos <82002471+felipeddsantos@users.noreply.github.com>
2021-06-13 23:46:11 -0300

    final

--- Admin/portalAdmin.py
+++ Admin/portalAdmin.py
@@ -13,35 +13,74 @@
 # limitations under the License.
 """The Python implementation of the GRPC helloworld.Greeter server."""
 
-from concurrent import futures
-import logging
+#Removendo comunicação por mqttp
 
-import grpc
-import socket
-
-import helloworld_pb2
-import helloworld_pb2_grpc
-
-####################################
 #import paho.mqtt.client as mqtt
-import time
-
 #broker = "localhost"
 #client = mqtt.Client("admin")
 #print("connecting to broker")
 #client.connect(broker)
+
+from concurrent import futures
+import logging
+import grpc
+import socket
+import struct
+import helloworld_pb2
+import helloworld_pb2_grpc
+import time
+import threading
+
 ####################################
 
 db = dict([])
 
-#Comunicação com o processo Cliente.java responsável pelo ratis
-
-def ratisConn(data):
+#Enviando atualização do banco para o processo responsável pelo ratis
+#Informações enviadas para em (127.0.0.1, 5100)
+def ratisSend(data):
   
-    ratis_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ratis_socket.connect(("localhost", 5100))
-    ratis_socket.send(data.encode())
-    ratis_socket.close()
+    s_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s_socket.connect(("localhost", 5100))
+    s_socket.send(data.encode())
+    s_socket.close()
+
+#Recebendo atualização do banco do processo responsável pelo Ratis, requisitado pelos outros portais
+#Informações recebidas em (224.1.1.1, 5101)
+def ratisReceive():
+  
+  while True:
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(("", 5101))
+
+    req = struct.pack("=4sl", socket.inet_aton("224.1.1.1"), socket.INADDR_ANY)
+    s.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, req)
+    data = s.recvfrom(1024)
+    updateData(data)
+
+#Atualização do banco de dados
+def updateData(data):
+   
+   global db
+
+   msg = []
+   msg = str(data).split(",")
+   
+   #Extraindo informações
+   msg[0] = msg[0].split("'")[1]
+   msg[1] = int(msg[1].split("'")[0])
+   msg[2] = msg[2].split("'")[0]
+   
+   if msg[0] == "add_client":
+    if msg[1] not in db.keys():
+      db[msg[1]] = msg[2]
+   elif msg[0] == "update_client":
+    db[msg[1]] = msg[2]
+   elif msg[0] == "delete_client":
+    del db[msg[1]]
+      
+   print(db)
 
 class Greeter(helloworld_pb2_grpc.GreeterServicer):
 
@@ -55,30 +94,30 @@ class Greeter(helloworld_pb2_grpc.GreeterServicer):
     if request.id in db.keys():
       return helloworld_pb2.HelloReply(message='Error! Already exists a client with ID = %s' % request.id)
     print("Inserting new Client "+request.name+" with ID = "+str(request.id))
-    db[request.id] = request.name
-    print(db)
-
+    
+    #db[request.id] = request.name
+    #print(db)
     #client.loop_start()
     #print("\nPublishing message to topic","/data") 
     #client.publish("/data", payload="I,"+str(request.id)+","+str(db[request.id]))
     #client.loop_stop()
 
-    ratisConn("add,"+str(request.id)+","+str(db[request.id]) + ",/n")
+    ratisSend("add_client,"+str(request.id)+","+str(request.name) + ",/n")
     
     return helloworld_pb2.HelloReply(message='Successfully created client with CID = %s!' % request.id)
   
   def updateClient(self,request,context):
     if request.id not in db.keys():
       return helloworld_pb2.HelloReply(message='Error! Client with CID = %s not found.' % request.id)
-    db[request.id] = request.name
-    print(db)
-
+    
+    #db[request.id] = request.name
+    #print(db)
     #client.loop_start()
     #print("\nPublishing message to topic","/data") 
     #client.publish("/data", payload="U,"+str(request.id)+","+str(db[request.id]))
     #client.loop_stop()
     
-    ratisConn("update,"+str(request.id)+","+str(db[request.id]) + ",/n")
+    ratisSend("update_client,"+str(request.id)+","+str(request.name) + ",/n")
     
     return helloworld_pb2.HelloReply(message='Successfully updated client with CID = %s!' % request.id)
 
@@ -91,19 +130,23 @@ class Greeter(helloworld_pb2_grpc.GreeterServicer):
   def deleteClient(self, request, context):
     if request.id not in db.keys():
       return helloworld_pb2.HelloReply(message='Error! Client with CID = %s not found.' % request.id)
-    del db[request.id]
-
+    
+    #del db[request.id]
     #client.loop_start()
     #print("\nPublishing message to topic","/data") 
     #client.publish("/data", payload="D,"+str(request.id))
     #client.loop_stop()
     
-    ratisConn("delete,"+str(request.id)+",/n")
+    ratisSend("delete_client,"+str(request.id)+",/n")
 
     return helloworld_pb2.HelloReply(message='Successfully deleted client with CID = %s!' % request.id)
 
-
 def serve():
+  
+    #Thread para comunicação com ratis
+    threadsub = threading.Thread(target=ratisReceive, args=())
+    threadsub.start()
+   
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
     server.add_insecure_port('[::]:50051')
